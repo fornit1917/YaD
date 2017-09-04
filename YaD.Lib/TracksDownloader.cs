77923da52754668848f5b886b7a04b3a4415022d
Vit S <fornit1917@gmail.com>
2017-09-04 22:54:43 +0300

    TracksDownloader, begin

---
+++ YaD.Lib/TracksDownloader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaD.Lib
+{
+    class TracksDownloader
+    {
+        private ITrackList tracks;
+        private IFileDownloader fileDownloader;
+        private IFileSystem fileSystem;
+
+        public TracksDownloader(ITrackList tracks)
+        {
+            this.tracks = tracks;
+        }
+
+        public TracksDownloader(ITrackList tracks, IFileDownloader fileDownloader, IFileSystem fileSystem) : this(tracks)
+        {
+            this.fileDownloader = fileDownloader;
+            this.fileSystem = fileSystem;
+        }
+
+        public void StartDownload(bool createAlbumFolders, int threadCount = 3)
+        {
+
+        }
+    }
+}
